3d7c3d90c9c330b60ee5f765843d5aac15241796
HemantSakhare <sakhare.hemant@gmail.com>
2019-05-05 11:50:16 +0530

    Changes done in Test1 file

---
+++ HelloWorld_PushFileToGit/src/main/java/com/example/HelloWorld_PushFileToGit/HelloWorldPushFileToGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.HelloWorld_PushFileToGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldPushFileToGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldPushFileToGitApplication.class, args);
+	}
+
+}
